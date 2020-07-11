49e970e396c87c8e815c9f03abc9bb2ddc9d9bed
Ken Li <ken.li.it@gmail.com>
2020-07-10 21:15:24 -0600

    C# codes

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRSA2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            RSACryptoServiceProvider csp = new RSACryptoServiceProvider(2048);
+            csp.FromXmlString(File.ReadAllText("./public.xml"));
+
+            // encrypting a string for testing purposes
+            byte[] plainText = Encoding.ASCII.GetBytes("Hello from CSharp");
+            byte[] cipherText = csp.Encrypt(plainText, false);
+
+            Console.WriteLine("Encrypted: " + Convert.ToBase64String(cipherText));
+            Console.ReadKey();
+        }
+    }
+}
